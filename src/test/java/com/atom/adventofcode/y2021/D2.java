a8924a92e748e097b2936edfd5de5583ec16905f
Alex Mooney <atmooney@gmail.com>
2021-12-01 18:37:43 +0000

    Ready for tomorrow

---
+++ src/test/java/com/atom/adventofcode/y2021/D2.java
@@ -0,0 +1,26 @@
+package com.atom.adventofcode.y2021;
+
+import com.atom.adventofcode.common.FileReader;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ *
+ */
+public class D2 {
+
+    @Test
+    public void testDayOnePart1() {
+//        assertEquals(7, countDepthIncreases(depth));
+//        assertEquals(1167,
+//                countDepthIncreases(FileReader.readFileIntegerList("src/test/resources/2021/D1.txt")));
+    }
+
+    @Test
+    public void testDayOnePart2() {
+//        assertEquals(5, countDepthIncreasesWithSlidingWindow(depth));
+//        assertEquals(1130,
+//                countDepthIncreasesWithSlidingWindow(FileReader.readFileIntegerList("src/test/resources/2021/D1.txt")));
+    }
+}
