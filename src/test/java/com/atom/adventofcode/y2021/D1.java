412fc725e6ef302b21b73b4d8455b54c8a475be5
Alex Mooney <atmooney@gmail.com>
2021-12-01 04:55:03 +0000

    Initial commit

---
+++ src/test/java/com/atom/adventofcode/y2021/D1.java
@@ -0,0 +1,19 @@
+package com.atom.adventofcode.y2021;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ *
+ */
+public class D1 {
+
+    @Test
+    public void testDayOnePart1() {
+    }
+
+    @Test
+    public void testDayOnePart2() {
+    }
+
+}
