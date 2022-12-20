32311e351f558862622030b01831270efd0f8b36
Alex Mooney <atmooney@gmail.com>
2022-12-20 10:12:49 +0000

    wip

--- src/test/java/com/atom/adventofcode/y2022/D20.java
+++ src/test/java/com/atom/adventofcode/y2022/D20.java
@@ -9,18 +9,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class D20 {
 
-    class Node {
-        Integer value;
+    static class Node {
+        Long value;
         Node next, prev;
-        public Node(int value, Node prev) {
+        public Node(long value, Node prev) {
             this.value = value;
             this.prev = prev;
         }
     }
 
-    private Node createdLinkedList(List<Integer> copy) {
+    private Node createdLinkedList(List<Long> copy) {
         Node prev = null, first = null;
-        for(Integer i : copy) {
+        for(Long i : copy) {
             Node n = new Node(i, prev);
             if(prev != null)
                 prev.next = n;
@@ -33,17 +33,7 @@ public class D20 {
         return first;
     }
 
-    private List<Integer> createList(Node first, int size) {
-        List<Integer> res = new ArrayList<>();
-        Node current = first;
-        for(int i=0; i<size; i++) {
-            res.add(current.value);
-            current = current.next;
-        }
-        return res;
-    }
-
-    private Node mixing(final List<Integer> nums) {
+    private Node mix(final List<Long> nums, int loops) {
         Node first = createdLinkedList(nums);
         List<Node> initialLocation = new ArrayList<>();
 
@@ -53,26 +43,23 @@ public class D20 {
             current = current.next;
         }
 
-        int listSize = nums.size();
+        for(int loop=0; loop < loops; loop++) {
+            for (Node n : initialLocation) {
+                long remainder = n.value % (nums.size() - 1);
 
-        for(Node n : initialLocation) {
+                if (first == n)
+                    first = first.next;
 
-            int remainder = n.value % listSize;
-//            int remainder = n.value;
-            Node p = n;
-            if(first == p) {
-                first = first.next;
-            }
-            if(remainder > 0) {
-                for(int i=0; i<remainder+1; i++) {
-                    p = p.next;
+                Node p = n;
+                if (remainder > 0) {
+                    for (int i = 0; i < remainder + 1; i++)
+                        p = p.next;
+                    swapOut(n, p);
+                } else if (remainder < 0) {
+                    for (int i = 0; i < Math.abs(remainder); i++)
+                        p = p.prev;
+                    swapOut(n, p);
                 }
-                swapOut(n, p);
-            } else if(remainder < 0){
-                for(int i=0; i<Math.abs(remainder); i++) {
-                    p = p.prev;
-                }
-                swapOut(n, p);
             }
         }
 
@@ -80,8 +67,6 @@ public class D20 {
     }
 
     private void swapOut(Node from, Node to) {
-
-        // cut out from
         from.prev.next = from.next;
         from.next.prev = from.prev;
 
@@ -91,12 +76,11 @@ public class D20 {
         to.prev = from;
     }
 
-    private int groveCoords(Node node) {
-        // find 0
+    private long groveCoords(Node node) {
         while(node.value != 0){ node = node.next; }
 
-        int sum = 0;
-        int c=0;
+        long sum = 0;
+        long c=0;
         while(c != 1000){ node = node.next; c++; }
         sum += node.value;
         while(c != 2000){ node = node.next; c++; }
@@ -108,15 +92,28 @@ public class D20 {
 
     @Test
     public void testEnc() {
-//        Node node = mixing(
-//                Arrays.asList(FileReader.readFileIntegerArray("src/test/resources/2022/D20_t.txt")));
-//        assertEquals(3, groveCoords(node));
+        List<Long> nums = Arrays.stream(FileReader.readFileIntegerArray("src/test/resources/2022/D20_t.txt"))
+                .map(i -> (long) i).toList();
+        assertEquals(3, groveCoords(mix(nums, 1)));
 
-        // -11286
-        Node node = mixing(
-                Arrays.asList(FileReader.readFileIntegerArray("src/test/resources/2022/D20.txt")));
-        assertEquals(0, groveCoords(node));
-
+        nums = Arrays.stream(FileReader.readFileIntegerArray("src/test/resources/2022/D20.txt"))
+                .map(i -> (long) i).toList();
+        assertEquals(11037, groveCoords(mix(nums, 1)));
     }
 
+    @Test
+    public void testEnc2() {
+        List<Long> nums = Arrays.stream(FileReader.readFileIntegerArray("src/test/resources/2022/D20_t.txt"))
+                .map(i -> (long) i).toList();
+
+        List<Long> biggerNums = nums.stream().map(l -> l * 811589153L).toList();
+        assertEquals(1623178306, groveCoords(mix(biggerNums, 10)));
+
+
+        nums = Arrays.stream(FileReader.readFileIntegerArray("src/test/resources/2022/D20.txt"))
+                .map(i -> (long) i).toList();
+
+        biggerNums = nums.stream().map(l -> l * 811589153L).toList();
+        assertEquals(3033720253914L, groveCoords(mix(biggerNums, 10)));
+    }
 }
