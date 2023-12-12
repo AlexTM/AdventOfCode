3fab1a27f9e6ea302ce65b7c5c8f421c1970d6c7
Alex Mooney <atmooney@gmail.com>
2023-12-12 22:42:28 +0000

    tidy

--- src/test/java/com/atom/adventofcode/y2023/D9.java
+++ src/test/java/com/atom/adventofcode/y2023/D9.java
@@ -18,8 +18,7 @@ public class D9 {
             10 13 16 21 30 45
             """;
 
-    private static long getDiff(List<Long> data) {
-
+    private static List<LinkedList<Long>> generateDiffs(List<Long> data) {
         List<LinkedList<Long>> diffs = new ArrayList<>();
         diffs.add(new LinkedList<>(data));
         while(data.stream().anyMatch(l -> l != 0)) {
@@ -30,15 +29,27 @@ public class D9 {
             diffs.add(diff);
             data = diff;
         }
+        return diffs;
+    }
 
-        long lastValue = 0;
+    private static long getDiffAtEnd(List<Long> data) {
+        List<LinkedList<Long>> diffs = generateDiffs(data);
         diffs.get(diffs.size()-1).addLast(0L);
         for(int i=diffs.size()-1; i>=1; i--) {
-            lastValue = diffs.get(i).getLast() + diffs.get(i-1).getLast();
-            diffs.get(i-1).addLast(lastValue);
+            diffs.get(i-1).addLast(
+                    diffs.get(i).getLast() + diffs.get(i-1).getLast());
         }
+        return diffs.get(0).getLast();
+    }
 
-        return lastValue;
+    private static long getDiffAtStart(List<Long> data) {
+        List<LinkedList<Long>> diffs = generateDiffs(data);
+        diffs.get(diffs.size()-1).addFirst(0L);
+        for(int i=diffs.size()-1; i>=1; i--) {
+            diffs.get(i-1).addFirst(
+                    diffs.get(i-1).getFirst() - diffs.get(i).getFirst());
+        }
+        return diffs.get(0).getFirst();
     }
 
     private List<Long> parseLine(String line) {
@@ -48,42 +59,18 @@ public class D9 {
     @Test
     public void partOne() {
         List<String> data = Arrays.stream(input.split("\n")).toList();
-        assertEquals(114, data.stream().mapToLong(s -> getDiff(parseLine(s))).sum());
+        assertEquals(114, data.stream().mapToLong(s -> getDiffAtEnd(parseLine(s))).sum());
 
         List<String> data2 = FileReader.readFileStringList("src/main/resources/2023/D9.txt");
-        assertEquals(2075724761L, data2.stream().mapToLong(s -> getDiff(parseLine(s))).sum());
-    }
-
-
-    private static long getDiff2(List<Long> data) {
-
-        List<LinkedList<Long>> diffs = new ArrayList<>();
-        diffs.add(new LinkedList<>(data));
-        while(data.stream().anyMatch(l -> l != 0)) {
-            LinkedList<Long> diff = new LinkedList<>();
-            for (int i = 0; i < data.size() - 1; i++) {
-                diff.add(data.get(i + 1) - data.get(i));
-            }
-            diffs.add(diff);
-            data = diff;
-        }
-
-        diffs.get(diffs.size()-1).addFirst(0L);
-        for(int i=diffs.size()-1; i>=1; i--) {
-            diffs.get(i-1).addFirst(
-                    diffs.get(i-1).getFirst() - diffs.get(i).getFirst());
-        }
-
-        return diffs.get(0).getFirst();
+        assertEquals(2075724761L, data2.stream().mapToLong(s -> getDiffAtEnd(parseLine(s))).sum());
     }
 
     @Test
     public void partTwo() {
         List<String> data = Arrays.stream(input.split("\n")).toList();
-        assertEquals(2, data.stream().mapToLong(s -> getDiff2(parseLine(s))).sum());
+        assertEquals(2, data.stream().mapToLong(s -> getDiffAtStart(parseLine(s))).sum());
 
         List<String> data2 = FileReader.readFileStringList("src/main/resources/2023/D9.txt");
-        assertEquals(1072, data2.stream().mapToLong(s -> getDiff2(parseLine(s))).sum());
+        assertEquals(1072, data2.stream().mapToLong(s -> getDiffAtStart(parseLine(s))).sum());
     }
-
 }
