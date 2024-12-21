d70d9aa2299065bbc7f2822f1cb0843d462e02d0
Alex Mooney <atmooney@gmail.com>
2024-12-21 22:38:39 +0000

    Day 21 done. very messy

--- src/test/java/com/atom/adventofcode/y2024/D21.java
+++ src/test/java/com/atom/adventofcode/y2024/D21.java
@@ -35,43 +35,37 @@ public class D21 {
             '*', '^', 'A',
             '<', 'v', '>'};
 
-    private List<String> intPath(Pos current, Pos end, String path, Character[] keypad) {
-        if(current.equals(end)) {
+    private List<String> generatePathForCombo(Pos current, Pos end, String path, Character[] keypad) {
+        if(current.equals(end))
             return List.of(path+"A");
-        }
-        if(keypad[current.x + current.y*3] == '*') {
+        if(keypad[current.x + current.y*3] == '*')
             return List.of();
-        }
 
-        int diffx = end.x - current.x;
-        int diffy = end.y - current.y;
+        int diffX = end.x - current.x;
+        int diffY = end.y - current.y;
         List<String> paths = new ArrayList<>();
-        if(diffx > 0) {
-            paths.addAll(intPath(new Pos(current.x+1, current.y), end, path + ">", keypad));
-        }
-        if(diffx < 0) {
-            paths.addAll(intPath(new Pos(current.x-1, current.y), end, path + "<", keypad));
-        }
-        if(diffy > 0) {
-            paths.addAll(intPath(new Pos(current.x, current.y+1), end, path + "v", keypad));
-        }
-        if (diffy < 0){
-            paths.addAll(intPath(new Pos(current.x, current.y-1), end, path + "^", keypad));
-        }
+        if(diffX > 0)
+            paths.addAll(generatePathForCombo(new Pos(current.x+1, current.y), end, path + ">", keypad));
+        if(diffX < 0)
+            paths.addAll(generatePathForCombo(new Pos(current.x-1, current.y), end, path + "<", keypad));
+        if(diffY > 0)
+            paths.addAll(generatePathForCombo(new Pos(current.x, current.y+1), end, path + "v", keypad));
+        if (diffY < 0)
+            paths.addAll(generatePathForCombo(new Pos(current.x, current.y-1), end, path + "^", keypad));
         return paths;
     }
 
-    private List<String> shortestPath(Character start, Character end, Character[] keypad) {
+    private List<String> generatePathForCombo(Character start, Character end, Character[] keypad) {
         int startIdx = IntStream.range(0, keypad.length).filter(i -> keypad[i] == start).findFirst().orElseThrow();
         int endIdx = IntStream.range(0, keypad.length).filter(i -> keypad[i] == end).findFirst().orElseThrow();
-        return intPath(new Pos(startIdx%3, startIdx/3), new Pos(endIdx%3, endIdx/3), "", keypad);
+        return generatePathForCombo(new Pos(startIdx%3, startIdx/3), new Pos(endIdx%3, endIdx/3), "", keypad);
     }
 
     private Map<Combo, List<String>> generateComboMapOfAllKeyCombinations(Character[] keypad) {
         Map<Combo, List<String>> comboMap = new HashMap<>();
         for (Character character : keypad) {
             for (Character value : keypad) {
-                comboMap.put(new Combo(character, value), shortestPath(character, value, keypad));
+                comboMap.put(new Combo(character, value), generatePathForCombo(character, value, keypad));
             }
         }
         return comboMap;
@@ -99,27 +93,52 @@ public class D21 {
         return result;
     }
 
-    private List<String> singleCombo(String target, final Map<Combo, List<String>> shortestPathKeyPad, final Map<Combo, List<String>> cache) {
-        return intSingleCombo("A"+target, 1, "", shortestPathKeyPad, cache);
-    }
+    record ComboCost(Combo combo, int depth) {}
+    private long costOfCombo(Combo combo, int depth, Map<Combo, List<String>> arrowPabCombos, Map<ComboCost, Long> cache) {
 
-    private int calculateComplexity(String target, Map<Combo, List<String>> numPadCombos, Map<Combo, List<String>> arrowPabCombos) {
-        List<String> comboForNumPad = singleCombo(target, numPadCombos, null);
-
-        List<String> result = new ArrayList<>();
-        for(String combo : comboForNumPad) {
-            List<String> combosForArrowPad1 = singleCombo(combo, arrowPabCombos, null);
-            for(String combo1 : combosForArrowPad1) {
-                List<String> combosForArrowPad2 = singleCombo(combo1, arrowPabCombos, new HashMap<>());
-                result.addAll(combosForArrowPad2);
-            }
+        if(depth == 0) {
+            return arrowPabCombos.get(combo).stream().mapToLong(String::length).min().orElseThrow();
         }
 
-        result.sort(Comparator.comparingInt(String::length));
-        int len = result.get(0).length();
-        int num = Integer.parseInt(target.substring(0, 3));
-        System.out.println("len: " + len + " num: " + num);
-        return len*num;
+        ComboCost cc = new ComboCost(combo, depth);
+        if(cache.containsKey(cc)) {
+            return cache.get(cc);
+        }
+
+        long minCost = Long.MAX_VALUE;
+        List<String> paths = arrowPabCombos.get(combo);
+        for(String path : paths) {
+            path = "A" + path;
+
+            long cost = 0;
+            for(int i=1; i<path.length(); i++) {
+                Combo c = new Combo(path.charAt(i-1), path.charAt(i));
+                cost += costOfCombo(c, depth-1, arrowPabCombos, cache);
+            }
+            minCost = Math.min(minCost, cost);
+        }
+
+        cache.put(cc, minCost);
+
+        return minCost;
+    }
+
+    private long calculateComplexity4(String target, int depth, Map<Combo, List<String>> numPadCombos, Map<Combo, List<String>> arrowPabCombos) {
+        List<String> comboForNumPad = intSingleCombo("AA"+target, 1, "", numPadCombos, null);
+        return calculateComplexity5(comboForNumPad, depth, numPadCombos, arrowPabCombos) * Long.parseLong(target.substring(0, 3));
+    }
+
+    private long calculateComplexity5(List<String> comboForNumPad, int depth, Map<Combo, List<String>> numPadCombos, Map<Combo, List<String>> arrowPabCombos) {
+        long minCost = Long.MAX_VALUE;
+        for(String path : comboForNumPad) {
+            long cost = 0;
+            for(int i=1; i<path.length(); i++) {
+                Combo c = new Combo(path.charAt(i-1), path.charAt(i));
+                cost += costOfCombo(c, depth-1, arrowPabCombos, new HashMap<>());
+            }
+            minCost = Math.min(minCost, cost);
+        }
+        return minCost;
     }
 
     @Test
@@ -127,16 +146,15 @@ public class D21 {
         Map<Combo, List<String>> numPadCombos = generateComboMapOfAllKeyCombinations(NUM_KEYPAD);
         Map<Combo, List<String>> arrowPabCombos = generateComboMapOfAllKeyCombinations(ARROW_KEYPAD);
 
-        assertEquals(126384, TEST_INPUT.lines().mapToInt(s -> calculateComplexity(s, numPadCombos, arrowPabCombos)).sum());
-        assertEquals(242484, INPUT.lines().mapToInt(s -> calculateComplexity(s, numPadCombos, arrowPabCombos)).sum());
+        assertEquals(126384, TEST_INPUT.lines().mapToLong(s -> calculateComplexity4(s, 2, numPadCombos, arrowPabCombos)).sum());
+        assertEquals(242484, INPUT.lines().mapToLong(s -> calculateComplexity4(s, 2, numPadCombos, arrowPabCombos)).sum());
     }
 
     @Test
     public void testPart2() {
         Map<Combo, List<String>> numPadCombos = generateComboMapOfAllKeyCombinations(NUM_KEYPAD);
         Map<Combo, List<String>> arrowPabCombos = generateComboMapOfAllKeyCombinations(ARROW_KEYPAD);
-
-        assertEquals(126384, TEST_INPUT.lines().mapToInt(s -> calculateComplexity(s, numPadCombos, arrowPabCombos)).sum());
-//        assertEquals(242484, INPUT.lines().mapToInt(s -> calculateComplexity(s, numPadCombos, arrowPabCombos)).sum());
+        assertEquals(294209504640384L,
+                INPUT.lines().mapToLong(s -> calculateComplexity4(s, 25, numPadCombos, arrowPabCombos)).sum());
     }
 }
